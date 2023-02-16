b694fab416e61824a6d0b89463cb38b5c817d752
santhosh2011 <santhosh2011@gmail.com>
2023-02-16 20:25:55 +0000

    Adding doc string

--- InterviewBit/Arrays/MinStepsinInfiniteGrid.java
+++ InterviewBit/Arrays/MinStepsinInfiniteGrid.java
@@ -1,3 +1,32 @@
+/***
+Author: Santhosh Thota
+
+Link: https://www.interviewbit.com/problems/min-steps-in-infinite-grid/
+
+Question : 
+
+You are in an infinite 2D grid where you can move in any of the 8 directions
+
+ (x,y) to 
+    (x-1, y-1), 
+    (x-1, y)  , 
+    (x-1, y+1), 
+    (x  , y-1),
+    (x  , y+1), 
+    (x+1, y-1), 
+    (x+1, y)  , 
+    (x+1, y+1) 
+You are given a sequence of points and the order in which you need to cover the points.. Give the minimum number of steps in which you can achieve it. You start from the first point.
+
+NOTE: This question is intentionally left slightly vague. Clarify the question by trying out a few cases in the “See Expected Output” section.
+
+INPUT FORMAT:
+Given two integer arrays A and B, where A[i] is x coordinate and B[i] is y coordinate of ith point respectively.
+
+
+ */
+
+
 import java.util.*;
 
 public class MinStepsinInfiniteGrid {
